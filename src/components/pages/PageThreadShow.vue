3b3551e0b082a462ff4b679988f6c4bbf20cc81e
marciomarquessouza <marciomultimedia@gmail.com>
2018-08-11 21:37:23 -0300

    added vue.set

--- src/components/pages/PageThreadShow.vue
+++ src/components/pages/PageThreadShow.vue
@@ -2,6 +2,18 @@
   <div class="col-large push-top">
     <h1>{{ thread.title }}</h1>
     <PostList :posts="posts" />
+    <form @submit.prevent="addPost">
+      <div class="form-group">
+            <textarea name=""
+                      rows="10"
+                      cols="30"
+                      class="form-input"
+                      v-model="newPostText"></textarea>
+      </div>
+      <div class="form-actions">
+        <button class="btn-blue">Submit post</button>
+      </div>
+    </form>
   </div>
 
 </template>
@@ -24,7 +36,24 @@
     data() {
       return {
         thread: sourceData.threads[this.id],
+        newPostText: '',
       };
+    },
+    methods: {
+      addPost() {
+        const postId = `great-post ${Math.random()}`;
+        const post = {
+          text: this.newPostText,
+          publishedAt: Math.floor(Date.now() / 1000),
+          threadId: this.id,
+          userId: 'jUjmgCurRRdzayqbRMO7aTG9X1G2',
+          '.key': postId,
+        };
+        this.$set(sourceData.posts, postId, post);
+        this.$set(this.thread.posts, postId, postId);
+        this.$set(sourceData.users[post.userId].posts, postId, postId);
+        this.newPostText = '';
+      },
     },
     computed: {
       posts() {
