2c11cb5b998a4365ca740b6571ec47fc7b95b3b3
marciomarquessouza <marciomultimedia@gmail.com>
2018-08-12 22:07:44 -0300

    added forum

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/components/pages/PageHome';
+import Forum from '@/components/pages/PageForum';
 import PageThreadShow from '@/components/pages/PageThreadShow';
 import NotFound from '@/components/pages/PageNotFound';
 
@@ -17,6 +18,12 @@ export default new Router({
       path: '/thread/:id',
       name: 'PageThreadShow',
       component: PageThreadShow,
+      props: true,
+    },
+    {
+      path: '/forum/:id',
+      name: 'Forum',
+      component: Forum,
       props: true,
     },
     {
