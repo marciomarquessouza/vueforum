e1037191578ff2da52de3d6b03701ad739895f8e
marciomarquessouza <marciomultimedia@gmail.com>
2018-08-19 22:18:02 -0300

    added auth user in nav-bar

--- src/components/TheNavBar.vue
+++ src/components/TheNavBar.vue
@@ -8,26 +8,22 @@
         </router-link>
 
         <div class="btn-hamburger">
-            <!-- use .btn-humburger-active to open the menu -->
             <div class="top bar"></div>
             <div class="middle bar"></div>
             <div class="bottom bar"></div>
         </div>
 
-    <!-- use .navbar-open to open nav -->
     <nav class="navbar">
         <ul>
             <li class="navbar-user">
                 <a href="#">
-                    <img class="avatar-small" src="https://pbs.twimg.com/profile_images/881260299420041217/GMVGlDea_400x400.jpg" alt="">
+                    <img class="avatar-small" :src="user.avatar" alt="">
                     <span>
-                        Alex Kyriakidis
+                        {{ user.name }}
                         <img class="icon-profile" src="assets/img/svg/arrow-profile.svg" alt="">
                     </span>
                 </a>
 
-                <!-- dropdown menu -->
-                <!-- add class "active-drop" to show the dropdown -->
                 <div id="user-dropdown">
                     <div class="triangle-drop"></div>
                     <ul class="dropdown-menu">
@@ -64,4 +60,13 @@
 
 <script>
 
+import { mapGetters } from 'vuex';
+
+export default {
+  computed: {
+    ...mapGetters({
+      user: 'authUser',
+    }),
+  },
+};
 </script>
